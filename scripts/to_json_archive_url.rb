0c441f93ad4bcd5d646664d4027ec21b0e6ff56b
ogasawaraShinnosuke <must.sow.before.you.can.reap+0001@gmail.com>
2016-04-14 14:58:05 +0900

    refactoring

--- scripts/to_json_archive_url.rb
+++ scripts/to_json_archive_url.rb
@@ -3,5 +3,4 @@
 require 'csv'
 require 'json'
 
-csv = CSV.open('../archive/url.csv', headers: true)
-p csv.map{|row|row.to_h}.to_json
+p CSV.open('../archive/url.csv', headers: true).map(&:to_h).to_json
